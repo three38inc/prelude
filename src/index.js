e0ca63cd2ee8598a0a05fe5747560c485eae8809
Jobith M Basheer <jobith@three38inc.com>
2020-05-24 18:10:58 +0530

    project-setup complete

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//  render engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// middleware
+app.use((req, res, next) => {
+    console.log(`We've got a hit 🔨 at ${new Date().toLocaleString()}`);
+    next();
+});
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Prelude',
+        seo: {
+            title: 'Prelude Home',
+            description: 'test description',
+        }
+    });
+});
+
+
+app.listen(port, () => console.log(`Prelude app 🔥🔥🔥 on port ${port}!`));
